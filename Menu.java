22cabc60ccb9f27a6928a530b42c85c989b4955b
Jhon <exneij27@gmail.com>
2025-03-27 12:19:23 -0500

    solucion validador, puse bandera = false en cada caso para verificar que ingresara en swich

--- Menu.java
+++ Menu.java
@@ -8,9 +8,12 @@ public class Menu {
         Scanner sc = new Scanner(System.in);
         boolean bandera = true;
         int opt = 0;
-        String mensaje = "Bienvenido al FastFoot perros llaneros y algomas! \n" + "Por favor Seleccione nuestro menú \n"
-                +
-                "1 : Perro Caliente \n" + "2 : Hamburguesa \n" + "3 : Perra \n" + "4 : Salchipapas \n"
+        String mensaje = "Bienvenido al FastFoot perros llaneros y algomas! \n"
+                + "Por favor Seleccione nuestro menú \n"
+                + "1 : Perro Caliente \n"
+                + "2 : Hamburguesa \n"
+                + "3 : Perra \n"
+                + "4 : Salchipapas \n"
                 + "5 : no quiero Nada";
 
         while (bandera) {
@@ -19,7 +22,10 @@ public class Menu {
                 System.out.println("Opcion no valida por favor intente de nuevo.....");
                 sc.next();
             }
-            while (sc.nextInt() > 5 || sc.nextInt() < 1) {
+            opt = sc.nextInt(); // al tener el scanner dentro del validador me lo pedia 2 veces y saltaba al
+                                // while principal asi fuera valido
+            while (opt > 5 || opt < 1) {
+                System.out.println();
                 System.out.println("Pagina en mantenimiento por favor escoja una opcion valida");
                 System.out.println(mensaje);
                 while (!sc.hasNextInt()) {
@@ -30,12 +36,17 @@ public class Menu {
             }
             switch (opt) {
                 case 1:
+
+                    bandera = false;
                     break;
                 case 2:
+                    bandera = false;
                     break;
                 case 3:
+                    bandera = false;
                     break;
                 case 4:
+                    bandera = false;
                     break;
                 case 5:
                     System.out.println("no compraste nada duro de paris");
